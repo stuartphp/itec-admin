ae19d220863b573fe11f55d7f528aaa61d4cbf08
stuartphp <stuart@ole.co.za>
2021-06-10 15:52:02 +0200

    2021-06-10

--- laravel/app/Http/Controllers/Admin/Products/DetailController.php
+++ laravel/app/Http/Controllers/Admin/Products/DetailController.php
@@ -5,12 +5,34 @@ namespace App\Http\Controllers\Admin\Products;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Models\ProductCategory;
+use App\Models\ProductUnit;
 
 class DetailController extends Controller
 {
     public function index($id)
     {
+        $units = $this->getUnits();
+        $categories = $this->getCategories();
+
         $data = Product::with(['category', 'units'])->where('company_id', session()->get('company_id'))->where('id', $id)->first();
-        return view('admin.products.details', compact('data'));
+        return view('admin.products.details', compact('data', 'categories', 'units'));
+    }
+
+    public function getCategories()
+    {
+        return ProductCategory::where('company_id', session()->get('company_id'))
+                ->where('is_active', 1)
+                ->orderBy('parent_id', 'desc')
+                ->orderBy('name', 'asc')
+                ->get();
+    }
+    public function getUnits()
+    {
+        return ProductUnit::where('company_id', session()->get('company_id'))
+                ->where('is_active', 1)
+                ->orderBy('name', 'asc')
+                ->pluck('name', 'id')
+                ->toArray();
     }
 }
